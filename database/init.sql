0d8bf3524498b0d1211a1d62d7d4659f9aa21971
Cristian Morales <cristian.morales1@unmsm.edu.pe>
2024-12-25 11:34:53 -0500

    finalizando controllers

--- database/init.sql
+++ database/init.sql
@@ -3,3 +3,11 @@ Create table Tasks (
     title VARCHAR(255) UNIQUE NOT NULL,
     description TEXT,
 );
+CREATE TABLE users(
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(255)  NOT NULL,
+    email   VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    created_at Timestamp DEFAULT CURRENT_TIMESTAMP,
+    updated_at Timestamp DEFAULT CURRENT_TIMESTAMP
+);
--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,4 +1,24 @@
+import {pool} from '../db.js'
+import bcrypt from 'bcryptjs'
 export const signin = (req, res) => res.send('Ingresando')
-export const signup = (req, res) => res.send('Registrando')
+
+export const signup = async (req, res) => {
+
+ try{
+    const {username, email, password} = req.body
+    
+    const hashedPassword = await bcrypt.hash(password, 10)
+    console.log(hashedPassword)
+    
+    const result = await pool.query('INSERT INTO users (username, email, password) VALUES ($1, $2, $3) returning *', [username, email, hashedPassword],)
+    console.log(result.rows[0])
+    res.send(result.rows[0]);
+    
+ } catch (error) {
+if (error.code === '23505') {
+    res.status(400).send('El usuario ya existe')
+ }   
+}}
+
 export const signout = (req, res) => res.send('Saliendo')
 export const profile = (req, res) => res.send('Perfil')
